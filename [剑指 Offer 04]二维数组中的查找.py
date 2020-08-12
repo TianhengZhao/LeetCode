51610e3bee80b6151a2ac02ebd707d03183b2197
TianhengZhao <1146824110@qq.com>
2020-08-12 22:56:06 +0800

    数组

---
+++ [剑指 Offer 04]二维数组中的查找.py
@@ -0,0 +1,58 @@
+# 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，
+# 判断数组中是否含有该整数。 
+# 
+#  
+# 
+#  示例: 
+# 
+#  现有矩阵 matrix 如下： 
+# 
+#  [
+#   [1,   4,  7, 11, 15],
+#   [2,   5,  8, 12, 19],
+#   [3,   6,  9, 16, 22],
+#   [10, 13, 14, 17, 24],
+#   [18, 21, 23, 26, 30]
+# ]
+#  
+# 
+#  给定 target = 5，返回 true。 
+# 
+#  给定 target = 20，返回 false。 
+# 
+#  
+# 
+#  限制： 
+# 
+#  0 <= n <= 1000 
+#  0 <= m <= 1000 
+# 
+#  
+# 
+#  注意：本题与主站 240 题相同：https://leetcode-cn.com/problems/search-a-2d-matrix-ii/ 
+#  Related Topics 数组 双指针
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+from typing import List
+
+
+class Solution:
+    def findNumberIn2DArray(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
+        n, m = len(matrix), len(matrix[0])
+        # 从右上角开始比较
+        row, col = 0, m - 1
+        while row < n and 0 <= col:
+            # 小于target，去掉该行
+            if target > matrix[row][col]:
+                row += 1
+            # 大于target，去掉该列
+            elif target < matrix[row][col]:
+                col -= 1
+            else:
+                return True
+        return False
+
+# leetcode submit region end(Prohibit modification and deletion)
