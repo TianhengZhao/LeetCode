e757f2cc6f7f03ae44371c12652839e390df654f
TianhengZhao <1146824110@qq.com>
2020-10-28 19:53:22 +0800

    栈

---
+++ [20]有效的括号.py
@@ -0,0 +1,59 @@
+# 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。 
+# 
+#  有效字符串需满足： 
+# 
+#  
+#  左括号必须用相同类型的右括号闭合。 
+#  左括号必须以正确的顺序闭合。 
+#  
+# 
+#  注意空字符串可被认为是有效字符串。 
+# 
+#  示例 1: 
+# 
+#  输入: "()"
+# 输出: true
+#  
+# 
+#  示例 2: 
+# 
+#  输入: "()[]{}"
+# 输出: true
+#  
+# 
+#  示例 3: 
+# 
+#  输入: "(]"
+# 输出: false
+#  
+# 
+#  示例 4: 
+# 
+#  输入: "([)]"
+# 输出: false
+#  
+# 
+#  示例 5: 
+# 
+#  输入: "{[]}"
+# 输出: true 
+#  Related Topics 栈 字符串
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2:
+            return False
+        dic = {'(': ')', '[': ']', '{': '}'}
+        stack = []
+        for braket in s:
+            # 右括号
+            if braket not in dic:
+                # 栈为空或不匹配
+                if not stack or dic[stack.pop()] != braket:
+                    return False
+            else:
+                stack.append(braket)
+        return not stack
+# leetcode submit region end(Prohibit modification and deletion)
